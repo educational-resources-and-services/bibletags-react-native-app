d70977e472fb465b57e2a284aec8ac0fbfa7097f
Andy Hubert <andy.hubert@gmail.com>
2019-06-13 16:31:33 +0300

    Basic render of usfm

--- src/components/major/ReadText.js
+++ src/components/major/ReadText.js
@@ -8,20 +8,58 @@ import { executeSql } from '../../utils/toolbox.js'
 import { getValidFontName } from "../../utils/bibleFonts.js"
 import RecentRef from '../basic/RecentRef'
 import RecentSearch from '../basic/RecentSearch'
+import { getPiecesFromUSFM, blockUsfmMarkers, tagInList } from "bibletags-ui-helper/src/splitting.js"
+import bibleVersions from '../../../versions.js'
 
 const {
   DEFAULT_FONT_SIZE,
 } = Constants.manifest.extra
 
-const styles = StyleSheet.create({
+const viewStyles = StyleSheet.create({
   container: {
   },
+  sup: {
+    position: "relative",
+    top: "-0.3em",
+  },
 })
+
+const textStyles = StyleSheet.create({
+  nd: {
+    // fontVariant: "small-caps",
+  },
+  em: {
+    fontStyle: "italic",
+  },
+  bd: {
+    fontWeight: "bold",
+  },
+  it: {
+    fontStyle: "italic",
+  },
+  bdit: {
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+  no: {
+    // fontVariant: "normal",
+    fontStyle: "normal",
+    fontWeight: "normal",
+  },
+  sc: {
+    // fontVariant: "small-caps",
+  },
+  sup: {
+    // fontSize: ".83em",
+  },
+})
+
+const getStyle = ({ tag, styles }) => styles[(tag || "").replace(/^\+/, '')]
 
 class ReadText extends React.PureComponent {
 
   state = {
-    verses: null,
+    pieces: null,
   }
 
   componentDidMount() {
@@ -41,39 +79,129 @@ class ReadText extends React.PureComponent {
       ],
     })
 
-    this.setState({ verses })
+    let wordDividerRegex
+
+    bibleVersions.some(version => {
+      if(version.id === versionId) {
+        wordDividerRegex = version.wordDividerRegex
+      }
+    })
+
+    const pieces = getPiecesFromUSFM({
+      usfm: verses.map(({ usfm }) => usfm).join('\n'),
+      wordDividerRegex,
+    })
+
+    this.setState({ pieces })
     // TODO: handle scrollY
   }
 
+  getJSXFromPieces = ({ pieces, verse }) => {
+    const { displaySettings } = this.props
+
+    const { font, textSize } = displaySettings
+    const fontSize = DEFAULT_FONT_SIZE * textSize
+    const displaySettingsStyle = {
+      fontSize,
+      fontFamily: getValidFontName({ font }),
+      // TODO:
+      // getValidFontName({ font, variant: 'italic' })
+      // getValidFontName({ font, variant: 'bold' })
+
+    }
+
+// only do tags when needed (otherwise, do plain text)
+// can there be content and text?
+// display verse numbers
+// display chapter numbers when in a different chapter
+// get all tags working
+// speed up
+
+    verse = verse || 1
+
+    return pieces.map((piece, idx) => {
+      const { type, tag, text, content, children } = piece
+
+      if(children) {
+
+        let component = (
+          <Text
+            key={idx}
+            style={[
+              displaySettingsStyle,
+              getStyle({ tag, styles: textStyles }),
+            ]}
+          >
+            {this.getJSXFromPieces({
+              pieces: children,
+              verse,
+            })}
+          </Text>
+        )
+
+        if(tagInList({ tag, list: blockUsfmMarkers })) {
+          component = (
+            <View
+              key={idx}
+              style={[
+                displaySettingsStyle,
+                getStyle({ tag, styles: viewStyles }),
+              ]}
+            >
+              {component}
+            </View>
+          )
+        }
+
+        return component
+
+      // } else if(type === "word") {
+
+      //   return (
+      //     <Text
+      //       key={idx}
+      //       {getStyle({ tag, styles: textStyles })}
+      //     >
+      //       {text}
+      //     </Text>
+      //   )
+
+      } else if(text || content) {
+
+        if(text && !tag) return text
+
+        return (
+          <Text
+            key={idx}
+            style={[
+              displaySettingsStyle,
+              getStyle({ tag, styles: textStyles }),
+            ]}
+          >
+            {text || content}
+          </Text>
+        )
+
+      } else {
+        return null
+      }
+    })
+  }
 
   render() {
     const { displaySettings } = this.props
-    const { verses } = this.state
+    const { pieces } = this.state
 
     const { font, textSize } = displaySettings
     const fontSize = DEFAULT_FONT_SIZE * textSize
 
-    if(!verses) return null
+    if(!pieces) return null
 
     return (
       <View
-        style={styles.container}
+        style={viewStyles.container}
       >
-        {verses.map(({ loc, usfm, search }) => (
-          <Text
-            key={loc}
-            style={{
-              fontSize,
-              fontFamily: getValidFontName({ font }),
-            }}
-          >
-            {search}
-          </Text>
-        ))}
-          
-        {/* <Text style={{ fontSize, fontFamily: getValidFontName({ font, variant: 'italic' }) }}>italics text2</Text>
-        <Text style={{ fontSize, fontFamily: getValidFontName({ font, variant: 'bold' }) }}>bold text2</Text> */}
-
+        {this.getJSXFromPieces({ pieces })}
       </View>
     )
   }
