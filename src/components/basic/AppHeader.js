a4ab11efdce5ffa5bc88ed288e05dee822c0bc3b
Huberts <huberts@Hubertss-iMac.local>
2019-08-07 08:44:34 -0400

    high-contrast app header and status bar for android

--- src/components/basic/AppHeader.js
+++ src/components/basic/AppHeader.js
@@ -2,6 +2,8 @@ import React from "react"
 import Constants from "expo-constants"
 import { Header } from "native-base"
 import { Platform, StyleSheet, View, StatusBar } from "react-native"
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
 
 import nativeBasePlatformVariables from 'native-base/src/theme/variables/platform'
 import { getToolbarHeight, isIPhoneX } from '../../utils/toolbox.js'
@@ -58,6 +60,24 @@ const styles = StyleSheet.create({
         )
     ),
   },
+  contrastStatusBar: {
+    ...(
+      Platform.OS === 'android'
+        ? {
+          backgroundColor: 'black',
+        }
+        : {}
+    ),
+  },
+  contrast: {
+    ...(
+      Platform.OS === 'android'
+        ? {
+          backgroundColor: '#222222',
+        }
+        : {}
+    ),
+  },
 })
 
 class AppHeader extends React.Component {
@@ -66,7 +86,7 @@ class AppHeader extends React.Component {
   // Thus, this component is a hack to force it to render properly.
 
   render() {
-    const { hide, hideStatusBar, ...headerParams } = this.props
+    const { hide, hideStatusBar, displaySettings, ...headerParams } = this.props
 
     const style = {}
 
@@ -75,12 +95,17 @@ class AppHeader extends React.Component {
     }
 
     return (
-      <View style={!hide && styles.container}>
+      <View
+        style={[
+          !hide ? styles.container : null,
+          displaySettings.theme === 'high-contrast' ? styles.contrastStatusBar : null,
+        ]}
+      >
         {(!hideStatusBar && isIPhoneX) &&
           <IPhoneXBuffer />
         }
         <StatusBar
-          backgroundColor={ANDROID_STATUS_BAR_COLOR}  // This does not seem to work
+          // backgroundColor={ANDROID_STATUS_BAR_COLOR}  // This does not seem to work
           translucent={true}
           animated={!hideStatusBar}
           hidden={hideStatusBar && !isIPhoneX}
@@ -89,6 +114,7 @@ class AppHeader extends React.Component {
           {...headerParams}
           style={[
             styles.header,
+            displaySettings.theme === 'high-contrast' ? styles.contrast : null,
             (hideStatusBar ? styles.noStatusBarSpace : null),
             style,
           ]}
@@ -100,4 +126,12 @@ class AppHeader extends React.Component {
   }
 }
 
-export default AppHeader
+const mapStateToProps = ({ displaySettings }) => ({
+  displaySettings,
+})
+
+const matchDispatchToProps = dispatch => bindActionCreators({
+  // setRef,
+}, dispatch)
+
+export default connect(mapStateToProps, matchDispatchToProps)(AppHeader)
