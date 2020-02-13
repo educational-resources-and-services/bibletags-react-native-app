c9e2eeb28d485ed71de8c284cde22c7119c4f179
Huberts <huberts@Hubertss-iMac.lan>
2020-02-13 14:45:28 +0200

    Made FullScreenSpin a functional component using hooks

--- src/components/basic/FullScreenSpin.js
+++ src/components/basic/FullScreenSpin.js
@@ -19,28 +19,26 @@ const styles = StyleSheet.create({
   },
 })
 
-class FullScreenSpin extends React.PureComponent {
-  render() {
-    const { style, text, percentage } = this.props
-
-    return (
-      <View style={[
-        styles.spinnerContainer,
-        style,
-      ]}>
-        <View style={styles.spacer} />
-        {!!text &&
-          <View style={styles.textContainer}>
-            <Text>{text}</Text>
-          </View>
-        }
-        <Spin
-          percentage={percentage}
-        />
-        <View style={styles.spacer} />
+const FullScreenSpin = React.memo(({
+  style,
+  text,
+  percentage,
+}) => (
+  <View style={[
+    styles.spinnerContainer,
+    style,
+  ]}>
+    <View style={styles.spacer} />
+    {!!text &&
+      <View style={styles.textContainer}>
+        <Text>{text}</Text>
       </View>
-    )
-  }
-}
+    }
+    <Spin
+      percentage={percentage}
+    />
+    <View style={styles.spacer} />
+  </View>
+))
 
 export default FullScreenSpin
