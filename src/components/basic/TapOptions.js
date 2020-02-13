3a92c4aacce35685e91ca2a1764db8be9cc7e96f
Huberts <huberts@Hubertss-iMac.lan>
2020-02-13 17:18:42 +0200

    Made TapOptions a functional component with hooks

--- src/components/basic/TapOptions.js
+++ src/components/basic/TapOptions.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from "react-native"
 
+import { useDimensions } from 'react-native-hooks'
+
 const MAX_WIDTH_PER_BUTTON = 70
 
 const styles = StyleSheet.create({
@@ -24,46 +26,48 @@ const styles = StyleSheet.create({
   },
 })
 
-class TapOptions extends React.PureComponent {
+const TapOptions = React.memo(({
+  options,
+  centerX,
+  bottomY,
+  topY,
+}) => {
 
-  render() {
-    const { options, centerX, bottomY, topY } = this.props
+  const { width } = useDimensions().window
+  const MAX_WIDTH = MAX_WIDTH_PER_BUTTON * options.length
+  const sideBuffer = MAX_WIDTH/2 + 20
 
-    const { width, height } = Dimensions.get('window')
-    const MAX_WIDTH = MAX_WIDTH_PER_BUTTON * options.length
-    const sideBuffer = MAX_WIDTH/2 + 20
-
-    return (
-      <View
-        style={[
-          styles.container,
-          {
-            marginLeft: MAX_WIDTH / -2,
-            marginRight: MAX_WIDTH / -2,
-            width: MAX_WIDTH,
-            left: Math.min(Math.max(centerX, sideBuffer), width - sideBuffer),
-          },
-          bottomY != null ? { bottom: bottomY } : null,
-          bottomY == null ? { top: topY } : null,
-        ]}
-      >
-        <View style={styles.buttons}>
-          {options.map(({ label, action }) => (
-            <TouchableOpacity
-              onPress={action}
-              key={label}
-            >
-              <View style={styles.button}>
-                <Text style={styles.text}>
-                  {label}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          ))}
-        </View>
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          marginLeft: MAX_WIDTH / -2,
+          marginRight: MAX_WIDTH / -2,
+          width: MAX_WIDTH,
+          left: Math.min(Math.max(centerX, sideBuffer), width - sideBuffer),
+        },
+        bottomY != null ? { bottom: bottomY } : null,
+        bottomY == null ? { top: topY } : null,
+      ]}
+    >
+      <View style={styles.buttons}>
+        {options.map(({ label, action }) => (
+          <TouchableOpacity
+            onPress={action}
+            key={label}
+          >
+            <View style={styles.button}>
+              <Text style={styles.text}>
+                {label}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ))}
       </View>
-    )
-  }
-}
+    </View>
+  )
+
+})
 
 export default TapOptions
