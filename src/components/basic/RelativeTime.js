2393129dc46731abd26ebbab96f7fb944b21fbe5
Huberts <huberts@Hubertss-iMac.lan>
2020-02-13 16:32:46 +0200

    Made RelativeTime a functional component with hooks

--- src/components/basic/RelativeTime.js
+++ src/components/basic/RelativeTime.js
@@ -3,28 +3,28 @@ import { Text } from "react-native"
 import moment from 'moment'
 import { getLocale } from 'inline-i18n'
 
-class RelativeTime extends React.PureComponent {
+const RelativeTime = React.memo(({
+  time,
+  ...otherProps
+}) => {
 
-  render() {
-    const { time, ...otherProps } = this.props
+  const fourHoursDifference = 1000 * 60 * 60 * 4
+  let timeString
 
-    const fourHoursDifference = 1000 * 60 * 60 * 4
-    let timeString
+  moment.locale(getLocale())
 
-    moment.locale(getLocale())
-
-    if(Math.abs(Date.now() - time) < fourHoursDifference) {
-      timeString = moment(time).fromNow()
-    } else {
-      timeString = moment(time).calendar()
-    }
-
-    return (
-      <Text {...otherProps}>
-        {timeString}
-      </Text>
-    )
+  if(Math.abs(Date.now() - time) < fourHoursDifference) {
+    timeString = moment(time).fromNow()
+  } else {
+    timeString = moment(time).calendar()
   }
-}
+
+  return (
+    <Text {...otherProps}>
+      {timeString}
+    </Text>
+  )
+
+})
 
 export default RelativeTime
