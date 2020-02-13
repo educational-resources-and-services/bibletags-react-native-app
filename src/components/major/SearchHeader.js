f99ab330cd1ed546fa9ae02de2fb3fac01f287b7
Huberts <huberts@Hubertss-iMac.lan>
2020-02-13 17:45:49 +0200

    Made SearchHeader a functional component with hooks

--- src/components/major/SearchHeader.js
+++ src/components/major/SearchHeader.js
@@ -1,11 +1,12 @@
-import React from "react"
-import { StyleSheet, Text, Dimensions, Platform, I18nManager } from "react-native"
+import React, { useCallback } from "react"
+import { StyleSheet, Text, Platform, I18nManager } from "react-native"
 import { Title, Subtitle, Left, Right, Button, Body, Item, Input } from "native-base"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 
 import { debounce, getVersionInfo, isRTLText } from '../../utils/toolbox.js'
 import { i18n } from "inline-i18n"
+import { useDimensions } from 'react-native-hooks'
 
 import AppHeader from "../basic/AppHeader"
 import HeaderIcon from "../basic/HeaderIcon"
@@ -52,157 +53,164 @@ const styles = StyleSheet.create({
   },
 })
 
-class SearchHeader extends React.PureComponent {
+const SearchHeader = React.memo(({
+  navigation,
+  setEditing,
+  editedSearchString,
+  updateEditedSearchString,
+  editing,
+  numberResults,
 
-  updateSearchString = () => {
-    const { navigation, setEditing, editedSearchString } = this.props
-    
-    const searchString = 
-      editedSearchString
-        .replace(/  +/g, ' ')
-        .trim()
+  displaySettings,
+}) => {
 
-    if(searchString === '') return
+  const { searchString, versionId } = navigation.state.params
 
-    setEditing(false)
+  const updateSearchString = useCallback(
+    () => {
+      const searchString = 
+        editedSearchString
+          .replace(/  +/g, ' ')
+          .trim()
 
-    debounce(
-      navigation.setParams,
-      {
-        ...navigation.state.params,
-        searchString,
-        editOnOpen: false,
-      },
-    )
-  }
+      if(searchString === '') return
 
-  onCancel = () => {
-    const { navigation, setEditing, updateEditedSearchString } = this.props
-    const { searchString } = navigation.state.params
+      setEditing(false)
 
-    if(!searchString) {
-      this.onBackPress()
-      return
-    }
-
-    setEditing(false)
-
-    updateEditedSearchString(searchString)
-  }
-
-  editSearchString = () => {
-    const { setEditing } = this.props
-
-    setEditing(true)
-  }
-
-  onBackPress = () => {
-    const { navigation } = this.props
-    
-    debounce(navigation.goBack)
-    // debounce(navigation.goBack, navigation.state.params.pageKey)
-  }
-
-  render() {
-    const { navigation, editing, numberResults, editedSearchString,
-            updateEditedSearchString, displaySettings } = this.props
-
-    const { searchString, versionId } = navigation.state.params
-    const { abbr, languageId } = getVersionInfo(versionId)
-
-    const { theme } = displaySettings
-    const { width } = Dimensions.get('window')
-    const maxTitleWidth = width - 120
-
-    if(editing) {
-      return (
-        <AppHeader searchBar rounded>
-          <Left style={styles.searchBarLeft}>
-            <Button
-              transparent
-              onPress={this.onBackPress}
-            >
-              <HeaderIcon name={I18nManager.isRTL ? "arrow-forward" : "arrow-back"} />
-            </Button>
-          </Left>
-          <Item>
-            <HeaderIcon name="search" />
-            <Input
-              placeholder={i18n("Search")}
-              returnKeyType="search"
-              value={editedSearchString}
-              onChangeText={updateEditedSearchString}
-              onSubmitEditing={this.updateSearchString}
-              autoCapitalize="none"
-              autoCompleteType="off"
-              autoCorrect={true}
-              enablesReturnKeyAutomatically={true}
-              importantForAutofill="no"
-              autoFocus={true}
-              selectTextOnFocus={true}
-              style={styles.input}
-            />
-          </Item>
-          <Right style={styles.searchBarRight}>
-            <Button
-              transparent
-              onPress={this.onCancel}
-            >
-              <HeaderIcon name="close" />
-            </Button>
-          </Right>
-        </AppHeader>
+      debounce(
+        navigation.setParams,
+        {
+          ...navigation.state.params,
+          searchString,
+          editOnOpen: false,
+        },
       )
-    }
+    },
+    [ navigation, setEditing, editedSearchString ],
+  )
 
+  const onBackPress = useCallback(
+    () => {
+      debounce(navigation.goBack)
+      // debounce(navigation.goBack, navigation.state.params.pageKey)
+    },
+    [ navigation ],
+  )
+
+  const onCancel = useCallback(
+    () => {
+      if(!searchString) {
+        onBackPress()
+        return
+      }
+
+      setEditing(false)
+
+      updateEditedSearchString(searchString)
+    },
+    [ searchString, onBackPress, setEditing, updateEditedSearchString ],
+  )
+
+  const editSearchString = useCallback(
+    () => setEditing(true),
+    [ setEditing ],
+  )
+
+  const { abbr, languageId } = getVersionInfo(versionId)
+
+  const { theme } = displaySettings
+  const { width } = useDimensions().window
+  const maxTitleWidth = width - 120
+
+  if(editing) {
     return (
-      <AppHeader>
-        <Left style={styles.left}>
+      <AppHeader searchBar rounded>
+        <Left style={styles.searchBarLeft}>
           <Button
             transparent
-            onPress={this.onBackPress}
+            onPress={onBackPress}
           >
             <HeaderIcon name={I18nManager.isRTL ? "arrow-forward" : "arrow-back"} />
           </Button>
         </Left>
-        <Body>
-          <Title style={[
-            styles.title,
-            theme === 'low-light' ? styles.titleLowLight : null,
-            { width: maxTitleWidth },
-          ]}>
-            {I18nManager.isRTL ? `\u2067`: `\u2066`}
-            {i18n("“{{searchString}}”", {
-              searchString: isRTLText({ languageId, searchString }) ? `\u2067${searchString}\u2069` : `\u2066${searchString}\u2069`,
-            })}
-            {`  `}
-            <Text style={styles.versionAbbr}>{abbr}</Text>
-          </Title>
-          <Subtitle
-            style={[
-              styles.subtitle,
-              displaySettings.theme === 'high-contrast' ? styles.contrast : null,
-            ]}
-          >
-            {
-              numberResults === false
-                ? i18n("Searching...")
-                : i18n("{{num_results}} result(s)", { num_results: numberResults })
-            }
-          </Subtitle>
-        </Body>
-        <Right style={styles.right}>
+        <Item>
+          <HeaderIcon name="search" />
+          <Input
+            placeholder={i18n("Search")}
+            returnKeyType="search"
+            value={editedSearchString}
+            onChangeText={updateEditedSearchString}
+            onSubmitEditing={updateSearchString}
+            autoCapitalize="none"
+            autoCompleteType="off"
+            autoCorrect={true}
+            enablesReturnKeyAutomatically={true}
+            importantForAutofill="no"
+            autoFocus={true}
+            selectTextOnFocus={true}
+            style={styles.input}
+          />
+        </Item>
+        <Right style={styles.searchBarRight}>
           <Button
             transparent
-            onPress={this.editSearchString}
+            onPress={onCancel}
           >
-            <HeaderIcon name="search" />
+            <HeaderIcon name="close" />
           </Button>
         </Right>
       </AppHeader>
     )
   }
-}
+
+  return (
+    <AppHeader>
+      <Left style={styles.left}>
+        <Button
+          transparent
+          onPress={onBackPress}
+        >
+          <HeaderIcon name={I18nManager.isRTL ? "arrow-forward" : "arrow-back"} />
+        </Button>
+      </Left>
+      <Body>
+        <Title style={[
+          styles.title,
+          theme === 'low-light' ? styles.titleLowLight : null,
+          { width: maxTitleWidth },
+        ]}>
+          {I18nManager.isRTL ? `\u2067`: `\u2066`}
+          {i18n("“{{searchString}}”", {
+            searchString: isRTLText({ languageId, searchString }) ? `\u2067${searchString}\u2069` : `\u2066${searchString}\u2069`,
+          })}
+          {`  `}
+          <Text style={styles.versionAbbr}>{abbr}</Text>
+        </Title>
+        <Subtitle
+          style={[
+            styles.subtitle,
+            theme === 'high-contrast' ? styles.contrast : null,
+          ]}
+        >
+          {
+            numberResults === false
+              ? i18n("Searching...")
+              : i18n("{{num_results}} result(s)", { num_results: numberResults })
+          }
+        </Subtitle>
+      </Body>
+      <Right style={styles.right}>
+        <Button
+          transparent
+          onPress={editSearchString}
+        >
+          <HeaderIcon name="search" />
+        </Button>
+      </Right>
+    </AppHeader>
+  )
+
+})
 
 const mapStateToProps = ({ displaySettings }) => ({
   displaySettings,
