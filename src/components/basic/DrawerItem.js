4032494864486446e070760a69cd15a162476dde
Huberts <huberts@Hubertss-iMac.lan>
2020-02-13 14:43:15 +0200

    Made DrawerItem into a functional component, using hooks

--- src/components/basic/DrawerItem.js
+++ src/components/basic/DrawerItem.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback } from "react"
 import * as StoreReview from 'expo-store-review'
 import { Image, StyleSheet, Linking } from "react-native"
 import { ListItem, Body, Text } from "native-base"
@@ -23,99 +23,109 @@ const styles = StyleSheet.create({
   },
 })
 
-class DrawerItem extends React.PureComponent {
+const DrawerItem = ({
+  navigation,
+  type,
+  href,
+  text,
+  image,
+  imageWidth,
+  imageHeight,
+  onPress,
+  locales,
+}) => {
 
-  changeLanguage = () => {
-    const { navigation } = this.props
+  const changeLanguage = useCallback(
+    () => {
+      debounce(
+        navigation.navigate,
+        "LanguageChooser",
+      )
+    },
+    [ navigation ],
+  )
 
-    debounce(
-      navigation.navigate,
-      "LanguageChooser",
-    )
-  }
+  const goVersions = useCallback(
+    () => {
+      debounce(
+        navigation.navigate,
+        "Versions",
+      )
+    },
+    [ navigation ],
+  )
 
-  goVersions = () => {
-    const { navigation } = this.props
+  const goToURL = useCallback(
+    event => {
+      if(type === 'rate') {
+        href = StoreReview.storeUrl()
+      }
 
-    debounce(
-      navigation.navigate,
-      "Versions",
-    )
-  }
+      if(!href) return
 
-  goToURL = event => {
-    let { type, href } = this.props
-
-    if(type === 'rate') {
-      href = StoreReview.storeUrl()
-    }
-
-    if(!href) return
-
-    Linking.openURL(href).catch(err => {
-      console.log('ERROR: Request to open URL failed.', err)
-      navigation.navigate("ErrorMessage", {
-        message: i18n("Your device is not allowing us to open this link."),
+      Linking.openURL(href).catch(err => {
+        console.log('ERROR: Request to open URL failed.', err)
+        navigation.navigate("ErrorMessage", {
+          message: i18n("Your device is not allowing us to open this link."),
+        })
       })
-    })
-  }
+    },
+    [ type, href, navigation ],
+  )
 
-  render() {
-    const { text, image, imageWidth, imageHeight, onPress, type, href, locales } = this.props
+  if(locales && !locales.includes(getLocale())) return null
 
-    if(locales && !locales.includes(getLocale())) return null
+  let typeAction, typeText
 
-    let typeAction, typeText
-
-    switch(type) {
-      case 'language': {
-        typeText = i18n("Change app language")
-        typeAction = this.changeLanguage
-        break
-      }
-      case 'rate': {
-        typeText = i18n("Rate this app")
-        typeAction = this.goToURL
-        break
-      }
-      case 'versions': {
-        typeText = i18n("Bible version information")
-        typeAction = this.goVersions
-        break
-      }
+  switch(type) {
+    case 'language': {
+      typeText = i18n("Change app language")
+      typeAction = changeLanguage
+      break
     }
-
-    return (
-      <ListItem
-        {...((onPress || typeAction || href)
-          ? {
-            button: true,
-            onPress: onPress || typeAction || this.goToURL,
-          }
-          : {}
-        )}
-        style={styles.listItem}
-      >
-        <Body>
-          {!!(text || typeText) &&
-            <Text style={styles.text}>{text || typeText}</Text> 
-          }
-          {!!image &&
-            <Image
-              source={image}
-              style={[
-                styles.image,
-                {
-                  width: imageWidth,
-                  paddingBottom: imageHeight,
-                },
-              ]}
-            />
-          }
-        </Body>
-      </ListItem>
-    )
+    case 'rate': {
+      typeText = i18n("Rate this app")
+      typeAction = goToURL
+      break
+    }
+    case 'versions': {
+      typeText = i18n("Bible version information")
+      typeAction = goVersions
+      break
+    }
   }
+
+  return (
+    <ListItem
+      {...((onPress || typeAction || href)
+        ? {
+          button: true,
+          onPress: onPress || typeAction || goToURL,
+        }
+        : {}
+      )}
+      style={styles.listItem}
+    >
+      <Body>
+        {!!(text || typeText) &&
+          <Text style={styles.text}>{text || typeText}</Text> 
+        }
+        {!!image &&
+          <Image
+            source={image}
+            style={[
+              styles.image,
+              {
+                width: imageWidth,
+                paddingBottom: imageHeight,
+              },
+            ]}
+          />
+        }
+      </Body>
+    </ListItem>
+  )
+
 }
 
 export default DrawerItem
