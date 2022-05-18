4eb9334d520285cee42d3b2d973f71cd8dba3062
AndyHubert <andy.hubert@gmail.com>
2022-05-18 10:07:58 -0400

    Improved instructions

--- src/components/screens/VerseTaggerHelp.js
+++ src/components/screens/VerseTaggerHelp.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   },
   body: {
     paddingTop: 20,
-    paddingBottom: 150,
+    paddingBottom: 400,
     paddingHorizontal: 20,
     width: '100%',
   },
@@ -62,70 +62,91 @@ const VerseTaggerHelp = ({
 
         <View style={styles.point}>
           <Text style={styles.line}>
-            1. Tap an original language word, then its associated translation words. Repeat. Do this for the entire verse before submitting.
+            1. Short-tap an original language word, then all its associated translation words. Repeat. Do this for the entire verse before submitting.
           </Text>
           <Text style={styles.example}>
-            Eg. In Genesis 1:1, you would tap “<Text style={styles.hebrew}>בְּ</Text>” then “In”. Next, you would tap “<Text style={styles.hebrew}>רֵאשִׁ֖ית</Text>” then “beginning”. And so on until the end of the verse.
+            Eg. In Genesis 1:1, you would tap “<Text style={styles.hebrew}>בְּ</Text>” then “In.” Next, you would tap “<Text style={styles.hebrew}>רֵאשִׁ֖ית</Text>” then “beginning.” And so on until the end of the verse.
+          </Text>
+          <Text style={styles.example}>
+            Eg. In Romans 1:2, you would tap “<Text style={styles.greek}>ὃ</Text>” then “which.” Next, you would tap “<Text style={styles.greek}>προεπηγγείλατο</Text>” followed by tapping “he,” “promised,” and “beforehand.” Etc.
           </Text>
         </View>
 
         <View style={styles.point}>
           <Text style={styles.line}>
-            2. DO NOT tag the indefinite article in a translation.
+            2. Tag all translated words, including helping verbs and translations you disagree with.
           </Text>
           <Text style={styles.example}>
-            Eg. “<Text style={styles.greek}>ἄνθρωπος</Text>” in John 1:6 should be tagged to “man” even though it is translated “a man”.
+            Eg. In Genesis 2:5, “<Text style={styles.hebrew}>לֹ֨א הִמְטִ֜יר</Text>” is translated “had not caused it to rain.” In this case, “<Text style={styles.hebrew}>לֹ֨א</Text>” should be tagged to “not” and “<Text style={styles.hebrew}>הִמְטִ֜יר</Text>” should be tagged to “had ... caused it to rain.”
           </Text>
         </View>
 
         <View style={styles.point}>
           <Text style={styles.line}>
-            3. Leave words untagged when they are repeated in the translation or original language, but not in both.
+            3. DO NOT tag the indefinite article in a translation.
           </Text>
           <Text style={styles.example}>
-            Eg. In Genesis 9:12, where we find “<Text style={styles.hebrew}>בֵּינִ/י֙ וּ/בֵ֣ינֵי/כֶ֔ם</Text>” translated to “between me and you”, the second “<Text style={styles.hebrew}>בֵּין</Text>” (inflected to “<Text style={styles.hebrew}>בֵ֣ינֵי</Text>”) should be left untagged.
+            Eg. “<Text style={styles.greek}>ἄνθρωπος</Text>” in John 1:6 should be tagged to “man” even though it is translated “a man.”
           </Text>
         </View>
 
         <View style={styles.point}>
           <Text style={styles.line}>
-            4. Include a translation’s genitive or dative helper words (eg. “of” and “to” in English).
+            4. Leave words untagged when they are repeated in the translation or original language, but not in both.
           </Text>
           <Text style={styles.example}>
-            Eg. “<Text style={styles.greek}>τῶν ἀνθρώπων</Text>” in John 1:4 should be tagged to “of men”.
-          </Text>
-          <Text style={styles.example}>
-            Eg. “<Text style={styles.hebrew}>פְּנֵ֣י</Text>” in Genesis 1:2 should be tagged to “face of”. So also “<Text style={styles.hebrew}>ר֣וּחַ</Text>” from the same verse should be tagged to “Spirit of” (even though its form can be construct or absolute) because the translation interprets it to be in construct form.
+            Eg. In Genesis 9:12, where we find “<Text style={styles.hebrew}>בֵּינִ/י֙ וּ/בֵ֣ינֵי/כֶ֔ם</Text>” translated to “between me and you,” the second “<Text style={styles.hebrew}>בֵּין</Text>” (inflected to “<Text style={styles.hebrew}>בֵ֣ינֵי</Text>”) should be left untagged.
           </Text>
         </View>
 
         <View style={styles.point}>
           <Text style={styles.line}>
-            5. DO NOT include translation words indicated by grammatical constructions not found in the specific original word(s) you are currenting tagging.
+            5. Include a translation’s genitive or dative helper words (eg. “of” and “to” in English).
           </Text>
           <Text style={styles.example}>
-            Eg. From the previous point, note that “face of” and not “the face of” should be tagged to “<Text style={styles.hebrew}>פְּנֵ֣י</Text>”. The entire phrase is “<Text style={styles.hebrew}>פְּנֵ֥י הַ/מָּֽיִם</Text>” translated to “the face of the deep”. Without “<Text style={styles.hebrew}>הַ/מָּֽיִם</Text>” it is unclear whether “<Text style={styles.hebrew}>פְּנֵ֥י</Text>” is definite or not. Therefore, the “the” before “face of” should simply remain untagged.
+            Eg. “<Text style={styles.greek}>τῶν ἀνθρώπων</Text>” in John 1:4 should be tagged to “of men.”
+          </Text>
+          <Text style={styles.example}>
+            Eg. “<Text style={styles.hebrew}>פְּנֵ֣י</Text>” in Genesis 1:2 should be tagged to “face of.” So also “<Text style={styles.hebrew}>ר֣וּחַ</Text>” from the same verse should be tagged to “Spirit of” (even though its form can be construct or absolute) because context shows it to be in construct form.
           </Text>
         </View>
 
         <View style={styles.point}>
           <Text style={styles.line}>
-            6. Long press on original language words to select more than one at a time. Do this when multiple words correspond to the translation word(s).
+            6. DO NOT include translation words indicated by grammatical constructions not found in the specific original word(s) you are currenting tagging.
           </Text>
           <Text style={styles.example}>
-            Eg. “<Text style={styles.hebrew}>מ֥וֹת תָּמֽוּת</Text>” in Genesis 2:17 should be tagged to “shall surely die”. (It would be incorrect to say that “<Text style={styles.hebrew}>מ֥וֹת</Text>” is translated “surely” even though that is the effect of the infinitive absolute in this context.)
-          </Text>
-          <Text style={styles.example}>
-            Eg. “<Text style={styles.greek}>τὸν Θεόν</Text>” in John 1:1 should be tagged to “God”.
+            Eg. From the previous point, note that “face of” and not “the face of” should be tagged to “<Text style={styles.hebrew}>פְּנֵ֣י</Text>.” The entire phrase is “<Text style={styles.hebrew}>פְּנֵ֥י הַ/מָּֽיִם</Text>” translated to “the face of the deep.” Without “<Text style={styles.hebrew}>הַ/מָּֽיִם</Text>” it is unclear whether “<Text style={styles.hebrew}>פְּנֵ֥י</Text>” is definite or not. Therefore, the “the” before “face of” should simply remain untagged.
           </Text>
         </View>
 
         <View style={styles.point}>
           <Text style={styles.line}>
-            7. Only tag phrases to phrases when you must.
+            7. Long press on original language words to select more than one at a time. Do this when multiple words correspond to the translation word(s).
           </Text>
           <Text style={styles.example}>
-            Eg. In Genesis 9:14, we find “<Text style={styles.hebrew}>בְּ/עַֽנְנִ֥/י עָנָ֖ן</Text>” translated to “When I bring clouds”. “<Text style={styles.hebrew}>בְּ</Text>” should be tagged to “when” and “י” should be tagged to “I”. However, the entire phrase “<Text style={styles.hebrew}>עַֽנְנִ֥– עָנָ֖ן</Text>” should be tagged to “bring clouds” since the verb does not mean “to bring” but rather “to cloud”.
+            Eg. “<Text style={styles.hebrew}>מ֥וֹת תָּמֽוּת</Text>” in Genesis 2:17 should be tagged to “shall surely die.” (It would be incorrect to say that “<Text style={styles.hebrew}>מ֥וֹת</Text>” is translated “surely” even though that is the effect of the infinitive absolute in this context.)
+          </Text>
+          <Text style={styles.example}>
+            Eg. “<Text style={styles.greek}>τὸν Θεόν</Text>” in John 1:1 should be tagged to “God.”
+          </Text>
+        </View>
+
+        <View style={styles.point}>
+          <Text style={styles.line}>
+            8. Only tag phrases to phrases when you must.
+          </Text>
+          <Text style={styles.example}>
+            Eg. In Genesis 9:14, we find “<Text style={styles.hebrew}>בְּ/עַֽנְנִ֥/י עָנָ֖ן</Text>” translated to “When I bring clouds.” “<Text style={styles.hebrew}>בְּ</Text>” should be tagged to “when” and “י” should be tagged to “I.” However, the entire phrase “<Text style={styles.hebrew}>עַֽנְנִ֥– עָנָ֖ן</Text>” should be tagged to “bring clouds” since the verb does not mean “to bring” but rather “to cloud.”
+          </Text>
+        </View>
+
+        <View style={styles.point}>
+          <Text style={styles.line}>
+            9. DO NOT include a paragogic <Text style={styles.hebrew}>נ</Text> or <Text style={styles.hebrew}>ה</Text> in your tagging.
+          </Text>
+          <Text style={styles.example}>
+            Eg. For “<Text style={styles.hebrew}>תְּמֻתֽוּ/ן</Text>” in Genesis 3:4, only “<Text style={styles.hebrew}>תְּמֻתֽוּ</Text>” should be tagged to “you will die.” The “<Text style={styles.hebrew}>ן</Text>” should be left untagged.
           </Text>
         </View>
 
