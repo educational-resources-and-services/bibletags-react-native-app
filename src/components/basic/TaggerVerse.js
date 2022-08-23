cc5ff528a723dd6dc75970c3d9548fd50af0b578
AndyHubert <andy.hubert@gmail.com>
2022-08-23 13:53:49 -0400

    Prevent translation word being cut off when tagging with accessibility font size increase

--- src/components/basic/TaggerVerse.js
+++ src/components/basic/TaggerVerse.js
@@ -187,6 +187,7 @@ const TaggerVerse = ({
                     translationWordsThemedStyle,
                     (isSelected ? selectedTranslationWordThemedStyle : null),
                   ]}
+                  adjustsFontSizeToFit
                 >
                   {key && translationWordInfoByWordIdAndPartNumbers[key].map(({ word }) => word).join(standardWordDivider)}
                 </Text>
